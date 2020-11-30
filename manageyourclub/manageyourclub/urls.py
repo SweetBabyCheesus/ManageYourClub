dca29a7e3e13410a439a595faabad0bc4352ac20
Tobias Wenzel <tobias.wenzel@student.hs-rm.de>
2020-11-30 18:55:54 +0100

    Startseite angepasst

--- manageyourclub/manageyourclub/urls.py
+++ manageyourclub/manageyourclub/urls.py
@@ -15,12 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from users.views import login
-from django.views.generic.base import TemplateView
+from users.views import home_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('users.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('', home_view, name='home'),
 ]
--- manageyourclub/users/views.py
+++ manageyourclub/users/views.py
@@ -2,11 +2,18 @@ from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
+from django.shortcuts import redirect
+from django.views.generic.base import TemplateView
 
-def login(request):
+def login_view(request):
     return render(request, 'registration/login.html')
 
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
+
+def home_view(request, *args, **kwargs):
+    if request.user.is_authenticated:
+        return TemplateView.as_view(template_name='home.html')(request)
+    return redirect(reverse_lazy('login'))
