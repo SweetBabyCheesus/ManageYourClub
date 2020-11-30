e033ca95ce5474edb8dc30ef3d8f09b7019f0e6e
mrose002 <maximilian.rosemeier@student.hs-rm.de>
2020-11-30 20:09:35 +0100

    MySQL migration

--- manageyourclub/manageyourclub/settings.py
+++ manageyourclub/manageyourclub/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'manageyourclub.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'manageYourClub$myc',
+        'HOST': 'manageYourClub.mysql.pythonanywhere-services.com',
+        'PORT': '3306',
+        'USER': 'manageYourClub',
+        'PASSWORD': 'nUdelhundER.187',
     }
 }
 
