4bb21721fcbb7797fc8f8144ff88a2a0f0768155
mrose002 <maximilian.rosemeier@student.hs-rm.de>
2020-12-04 00:06:47 +0100

    git sucks

--- manageyourclub/manageyourclub/settings.py
+++ manageyourclub/manageyourclub/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'manageyourclub.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -94,7 +94,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 } 
-"""
+
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
