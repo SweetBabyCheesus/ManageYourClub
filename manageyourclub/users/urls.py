42c874906cab6fb6481802dd7f88c51b14f4a70a
mrose002 <maximilian.rosemeier@student.hs-rm.de>
2020-12-03 23:22:39 +0100

    User-Registrierung v1

--- manageyourclub/users/urls.py
+++ manageyourclub/users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import SignUpView
+from .views import SignUpView, home_view
 
 
 urlpatterns = [
     path('signup/', SignUpView, name='signup'),
+    path('home/', home_view, name='home'),
 ]
--- manageyourclub/users/views.py
+++ manageyourclub/users/views.py
@@ -1,30 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
-from django.shortcuts import redirect
 from django.views.generic.base import TemplateView
+from django.contrib.auth import authenticate, login
+
+from .forms import CreateUserForm, CreateUserProfileForm
 
 def login_view(request):
     return render(request, 'registration/login.html')
 
 def SignUpView(request):
-    form = UserCreationForm()
 
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
+        
+        user_form = CreateUserForm(request.POST)
+        profile_form = CreateUserProfileForm(request.POST)
 
-    context  = {'form':form}
+        if user_form.is_valid() and profile_form.is_valid():
+            
+            user = user_form.save()
+
+            profile = profile_form.save(commit=False)
+            profile.user = user
+            profile.save()
+
+            username = user_form.cleaned_data.get('username')
+            password = user_form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return render(request,'home.html')
+
+    else:
+        user_form = CreateUserForm()
+        profile_form = CreateUserProfileForm()
+
+    context  = {'user_form':user_form, 'profile_form':profile_form}
     return  render(request,'registration/signup.html', context)
 
-"""
-    success_url = reverse_lazy('login')
-    template_name = 'registration/signup.html'
-"""
-def home_view(request, *args, **kwargs):
+
+def home_view(request):
     if request.user.is_authenticated:
-        return TemplateView.as_view(template_name='home.html')(request)
-    return redirect(reverse_lazy('login'))
+        return Trender(request,'home.html')
+    return Trender(request,'home.html')
