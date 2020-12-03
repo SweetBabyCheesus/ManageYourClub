da7d5538fa4cb9d30617b2250142eae8aefc4b8c
Jonas Stark <jonas.stark@student.hs-rm.de>
2020-12-03 17:15:46 +0100

    vorläufiger login mit richtiger db

--- manageyourclub/manageyourclub/settings.py
+++ manageyourclub/manageyourclub/settings.py
@@ -74,12 +74,26 @@ WSGI_APPLICATION = 'manageyourclub.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'manageYourClub$myc',
+        'HOST': 'manageYourClub.mysql.pythonanywhere-services.com',
+        'PORT': '3306',
+        'USER': 'manageYourClub',
+        'PASSWORD': 'nUdelhundER.187',
     }
+}
 } 
+
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#} 
 
 
 # Password validation
