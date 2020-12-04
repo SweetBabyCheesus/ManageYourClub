e1ded9a96d4f593fde4feb4887606b2c21811680
Jonas Stark <jonas.stark@student.hs-rm.de>
2020-12-04 12:44:36 +0100

    login form

--- manageyourclub/manageyourclub/settings.py
+++ manageyourclub/manageyourclub/settings.py
@@ -94,7 +94,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 } 
-
+"""
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
