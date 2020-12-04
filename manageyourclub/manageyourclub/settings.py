177dfe521c91d3adadbacd4226be98ebd5ad29fd
Jonas Stark <jonas.stark@student.hs-rm.de>
2020-12-04 12:16:41 +0100

    db

--- manageyourclub/manageyourclub/settings.py
+++ manageyourclub/manageyourclub/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'manageyourclub.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
