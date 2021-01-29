1d9cedc100287deaae9eeef7bf9fdb4b2719773b
mrose002 <maximilian.rosemeier@student.hs-rm.de>
2021-01-29 13:33:05 +0100

    Auskommentierte Methoden in clubs.forms gelöscht

--- manageyourclub/clubs/forms.py
+++ manageyourclub/clubs/forms.py
@@ -4,42 +4,6 @@ from clubs.models import ClubModel
 from clubs.models import AddressModel
 from clubs.models import PlaceModel
 
-# bisher nicht in Benutzung - ist im Lernprozess über Forms mit Foreign Keys enstanden
-# Klasse übernommen und abgeändert von:
-# https://www.codesd.com/item/how-to-create-forms-for-the-foreign-key-django.html
-#class PlaceForm(forms.ModelForm):
-#    """ModelForm für Orte"""
-#    postcode = forms.IntegerField(max_value=99999, min_value=0, label='PLZ')
-#    village = forms.CharField(max_length=20, label='Ort')
-#    class Meta:
-#        model = PlaceModel
-#        fields = ('postcode', 'village',)
-
-# bisher nicht in Benutzung - ist im Lernprozess über Forms mit Foreign Keys enstanden
-# Klasse übernommen und abgeändert von:
-# https://www.codesd.com/item/how-to-create-forms-for-the-foreign-key-django.html
-#class AddressForm(forms.ModelForm):
-#    """ModelForm für Adressen"""
-#    streetAddress = forms.CharField(max_length=20, label='Straße')
-#    houseNumber = forms.CharField(max_length=5, label='Hausnummer')
-#    postcode = forms.IntegerField(max_value=99999, min_value=0, label='PLZ')
-#    village = forms.CharField(max_length=20, label='Ort')
-    # class Meta:
-        # model = AddressModel
-        # fields = ('streetAddress',)
-
-    # def save(self, commit=True):
-        # """speichert die vom Nutzer eingegebenen Daten"""
-        # instance = super().save(commit=False)
-        # pc = self.cleaned_data['postcode']
-        # vil = self.cleaned_data['village']
-        # place, created = PlaceModel.objects.get_or_create(postcode=pc, village=vil)
-        # if created and commit:
-            # place.save()
-        # instance.postcode = place
-        # instance.save(commit)
-        # return instance
-
 # Klasse übernommen und abgeändert von:
 # https://www.codesd.com/item/how-to-create-forms-for-the-foreign-key-django.html
 class AddClubForm(forms.ModelForm):
