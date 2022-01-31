8add6eb8f5664092cf6ffcd5f2c04b32e0237537
max <maximilian.rosemeier@student.hs-rm.de>
2022-01-31 17:31:42 +0100

    DB zurückgesetzt, BUG Passwort reset behoben

--- manageyourclub/users/models.py
+++ manageyourclub/users/models.py
@@ -100,8 +100,8 @@ class CustomUser(AbstractBaseUser):
     #Erstellung der benötigten Datenfelder
     #Emailfeld fungiert als Username. Daher muss sie Unique sein
     email = models.EmailField(verbose_name='email', max_length = 60, unique=True)
-    date_joined = models.DateField(verbose_name='date joined', auto_now_add=True)
-    last_login = models.DateField(verbose_name='last login', auto_now=True)
+    date_joined = models.DateTimeField(verbose_name='date joined', auto_now_add=True)
+    last_login = models.DateTimeField(verbose_name='last login', auto_now=True)
     is_admin = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
